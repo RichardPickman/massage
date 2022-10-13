e84ef18eb8e27b2ab3ad2cd65cbe9fd4f9d801e4
RichardPickman <drezzerock@gmail.com>
2022-10-13 21:01:14 +0200

    feat: add main files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Quiz from "./components/Quiz";
+import './styles.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <Quiz />
+    </div>
+  )
+}
+
+export default App;
