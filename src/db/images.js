2c5ae01705c5a943582ede2eeef5d09c024990cf
RichardPickman <drezzerock@gmail.com>
2022-10-14 21:09:48 +0200

    feat: add pictures bank

---
+++ src/db/images.js
@@ -0,0 +1,136 @@
+import Brzegboczny from './img/Brzeg-boczny.jpg'
+import Brzeggorny from './img/Brzeg-gorny.jpg'
+import Brzegprzysrodkowy from './img/Brzeg-przysrodkowy.jpg'
+import C2 from './img/C-2.png'
+import C7TH1 from './img/C-7-TH-1.png'
+import L4 from './img/L-4.png'
+import Stawmostkowoobojczykowy from './img/Staw-mostkowo-obojczykowy.jpg'
+import TH3 from './img/TH-3.png'
+import TH7 from './img/TH-7.png'
+import Wcieciemostkoweobojczyka from './img/Wciecie-mostkowe-obojczyka.jpg'
+import bruzdamiedzyguzkowa from './img/bruzda-miedzyguzkowa.jpg'
+import bruzdamiedzyklykciowakosciudowej from './img/bruzda-miedzyklykciowa-kosci-udowej.png'
+import bruzdanerwupromieniowego from './img/bruzda-nerwu-promieniowego.jpg'
+import dolnadgrzebieniowy from './img/dol-nad-grzebieniowy.jpg'
+import dolpodgrzebieniowy from './img/dol-pod-grzebieniowy.jpg'
+import dolwyrostkalokciowego from './img/dol-wyrostka-lokciowego.jpg'
+import glowakoscipromieniowej from './img/glowa-kosci-promieniowej.jpg'
+import glowakoscistrzalkowej from './img/glowa-kosci-strzalkowej.png'
+import glowakosciudowej from './img/glowa-kosci-udowej.png'
+import grzebienkoscibiodrowej from './img/grzebien-kosci-biodrowej.png'
+import grzebienkrzyzowyposredni from './img/grzebien-krzyzowy-posredni.png'
+import grzebienlopatki from './img/grzebien-lopatki.jpg'
+import guzkulszowy from './img/guz-kulszowy.png'
+import guzpietowykoscipietowej from './img/guz-pietowy-kosci-pietowej.png'
+import guzeklistera from './img/guzek-listera.png'
+import guzeklonowy from './img/guzek-lonowy.png'
+import guzekmniejszy from './img/guzek-mniejszy.jpg'
+import guzekwiekszy from './img/guzek-wiekszy.jpg'
+import guzowatosckoscipiszczelowej from './img/guzowatosc-kosci-piszczelowej.png'
+import guzowatoscnaramienna from './img/guzowatosc-naramienna.jpg'
+import guzowatoscpotyliczna from './img/guzowatosc-potyliczna.png'
+import katboczny from './img/kat-boczny.jpg'
+import katdolny from './img/kat-dolny.jpg'
+import katgorny from './img/kat-gorny.jpg'
+import klykiecbocznypiszczelowej from './img/klykiec-boczny-piszczelowej.png'
+import klykiecboczny from './img/klykiec-boczny.png'
+import klykiecprzysrodkowypiszczelowej from './img/klykiec-przysrodkowy-piszczelowej.png'
+import klykiecprzysrodkowy from './img/klykiec-przysrodkowy.png'
+import kolecbiodrowyprzednigorny from './img/kolec-biodrowy-przedni-gorny.png'
+import kolecbiodrowytylnygorny from './img/kolec-biodrowy-tylny-gorny.png'
+import kosclonowa from './img/kosc-lonowa.png'
+import kostkabocznadwochkosci from './img/kostka-boczna-dwoch-kosci.png'
+import kostkaprzysrodkowadwochkosci from './img/kostka-przysrodkowa-dwoch-kosci.png'
+import kresakarkowagorna from './img/kresa-karkowa-gorna.png'
+import kretarzmniejszykosciudowej from './img/kretarz-mniejszy-kosci-udowej.png'
+import kretarzwiekszykosciudowej from './img/kretarz-wiekszy-kosci-udowej.png'
+import nadklykiecboczny from './img/nadklykiec-boczny.jpg'
+import nadklykiecprzysrodkowy from './img/nadklykiec-przysrodkowy.jpg'
+import obojczyk from './img/obojczyk.jpg'
+import potylica from './img/potylica.png'
+import rzepka from './img/rzepka.png'
+import spojenielonowe from './img/spojenie-lonowe.png'
+import stawbarkowoobojczykowy from './img/staw-barkowo-obojczykowy.jpg'
+import szczelinastawukolanowego from './img/szczelina-stawu-kolanowego.png'
+import trojkatgrzebenialopatki from './img/trojkat-grzebenia-lopatki.jpg'
+import trzonkoscipiszczelowej from './img/trzon-kosci-piszczelowej.png'
+import trzonkosciramiennej from './img/trzon-kosci-ramiennej.jpg'
+import trzonkoscistrzalkowej from './img/trzon-kosci-strzalkowej.png'
+import wyrostekbarkowylopatki from './img/wyrostek-barkowy-lopatki.jpg'
+import wyrostekdziobiastykoscilokciowej from './img/wyrostek-dziobiasty-kosci-lokciowej.jpg'
+import wyrostekkruczylopatki from './img/wyrostek-kruczy-lopatki.jpg'
+import wyrosteklokciowy from './img/wyrostek-lokciowy.jpg'
+import wyrostekpoprzecznyc1 from './img/wyrostek-poprzeczny-c1.png'
+import wyrostekrylcowatykoscilokciowej from './img/wyrostek-rylcowaty-kosci-lokciowej.png'
+import wyrostekrylcowatykoscipromieniowej from './img/wyrostek-rylcowaty-kosci-promieniowej.png'
+import wyrosteksutkowaty from './img/wyrostek-sutkowaty.png'
+
+
+export const images  = {
+  Brzegboczny,
+  Brzeggorny,
+  Brzegprzysrodkowy,
+  C2,
+  C7TH1,
+  L4,
+  Stawmostkowoobojczykowy,
+  TH3,
+  TH7,
+  Wcieciemostkoweobojczyka,
+  bruzdamiedzyguzkowa,
+  bruzdamiedzyklykciowakosciudowej,
+  bruzdanerwupromieniowego,
+  dolnadgrzebieniowy,
+  dolpodgrzebieniowy,
+  dolwyrostkalokciowego,
+  glowakoscipromieniowej,
+  glowakoscistrzalkowej,
+  glowakosciudowej,
+  grzebienkoscibiodrowej,
+  grzebienkrzyzowyposredni,
+  grzebienlopatki,
+  guzkulszowy,
+  guzpietowykoscipietowej,
+  guzeklistera,
+  guzeklonowy,
+  guzekmniejszy,
+  guzekwiekszy,
+  guzowatosckoscipiszczelowej,
+  guzowatoscnaramienna,
+  guzowatoscpotyliczna,
+  katboczny,
+  katdolny,
+  katgorny,
+  klykiecbocznypiszczelowej,
+  klykiecboczny,
+  klykiecprzysrodkowypiszczelowej,
+  klykiecprzysrodkowy,
+  kolecbiodrowyprzednigorny,
+  kolecbiodrowytylnygorny,
+  kosclonowa,
+  kostkabocznadwochkosci,
+  kostkaprzysrodkowadwochkosci,
+  kresakarkowagorna,
+  kretarzmniejszykosciudowej,
+  kretarzwiekszykosciudowej,
+  nadklykiecboczny,
+  nadklykiecprzysrodkowy,
+  obojczyk,
+  potylica,
+  rzepka,
+  spojenielonowe,
+  stawbarkowoobojczykowy,
+  szczelinastawukolanowego,
+  trojkatgrzebenialopatki,
+  trzonkoscipiszczelowej,
+  trzonkosciramiennej,
+  trzonkoscistrzalkowej,
+  wyrostekbarkowylopatki,
+  wyrostekdziobiastykoscilokciowej,
+  wyrostekkruczylopatki,
+  wyrosteklokciowy,
+  wyrostekpoprzecznyc1,
+  wyrostekrylcowatykoscilokciowej,
+  wyrostekrylcowatykoscipromieniowej,
+  wyrosteksutkowaty,
+}
