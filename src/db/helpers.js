c9bf1af54ce1524ecd7b6b9789d3b1f09ad09cf7
RichardPickman <drezzerock@gmail.com>
2022-10-18 20:52:09 +0200

    feate: update db

--- src/db/helpers.js
+++ src/db/helpers.js
@@ -18,33 +18,34 @@ const getFiles = async () => {
   const localPath = path.resolve(__dirname)
   const files = await fs.readdir(localPath + '/img')
 
-  for (const file of files) {
+  for (let i=0; i < files.length; i++) {
     // console.log(`import ${file.split('.')[0].split('-').join('')} from './img/${file}'`)
     console.log(`{
+      id: ${i},
       question: "",
-      img: images.${file.split('.')[0].split('-').join('')},
+      img: images.${files[i].split('.')[0].split('-').join('')},
       incorrectAnswers: [
-        '${getRandom(file, files).split('.')[0].split('-').join(' ')}',
-        '${getRandom(file, files).split('.')[0].split('-').join(' ')}',
-        '${getRandom(file, files).split('.')[0].split('-').join(' ')}',
+        '${getRandom(files[i], files).split('.')[0].split('-').join(' ')}',
+        '${getRandom(files[i], files).split('.')[0].split('-').join(' ')}',
+        '${getRandom(files[i], files).split('.')[0].split('-').join(' ')}',
       ],
-      correctAnswer: '${file.split('.')[0].split('-').join(' ')}',
+      correctAnswer: '${files[i].split('.')[0].split('-').join(' ')}',
     },`)
   }
 }
 
 const removeHash = async () => {
-  const localPath = path.resolve(__dirname)
-  const files = await fs.readdir(localPath + '/img')
+  const localPath = path.resolve(__dirname);
+  const files = await fs.readdir(localPath + '/img');
   const isCopyDirExist = files.includes('copy');
   
-  !isCopyDirExist && await fs.mkdir(path.join(localPath, 'img', 'copy'))
+  !isCopyDirExist && await fs.mkdir(path.join(localPath, 'img', 'copy'));
 
   for (const file of files) {
-    const imgPath = path.join(localPath, 'img')
+    const imgPath = path.join(localPath, 'img');
     const [name, _, format] = file.split('.');
 
-    copyFile(path.join(imgPath, file), path.join(imgPath, 'copy', `${name}.${format}`), (err) => err && console.log(err))
+    copyFile(path.join(imgPath, file), path.join(imgPath, 'copy', `${name}.${format}`), (err) => err && console.log(err));
   }
 
   return;
