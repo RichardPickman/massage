30fd78a1ce38411a6af2495c02f1a9fc936e9494
RichardPickman <drezzerock@gmail.com>
2022-10-29 21:21:50 +0200

    feat: add reducer

--- src/context/quizConstructor.js
+++ src/context/quizConstructor.js
@@ -1,21 +1,153 @@
 const { createContext, useReducer } = require("react");
 
+const questionInitialState = (id = 0) => ({
+  id,
+  question: "",
+  img: "",
+  answers: [''],
+  correctAnswers: [],
+  isSaved: false,
+})
+
 const initialState = {
   questions: [],
   title: '',
-  questionCount: 0,
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_QUESTION": {
-      return state;
+      const currentIndex = state.questions.length;
+      const temp = questionInitialState(currentIndex);
+      const questions = [...state.questions, temp];
+ 
+      return {
+        ...state,
+        questions,
+      }
     }
     case "REMOVE_QUESTION": {
-      return state;
+      const questions = state.filter(payload => payload.id !== state.questionId);
+
+      return {
+        ...state,
+        questions
+      }
     }
     case "EDIT_QUESTION": {
-      return state;
+      const questions = state.questions.map(quest => {
+        if (quest.id === state.payload.id) {
+          return state.payload
+        }
+
+        return quest;
+      });
+
+      return {
+        ...state,
+        questions,
+      };
+    }
+    case "SAVE_QUESTION": {
+      const questions = state.questions.map(quest => {
+        if (quest.id === action.payload.questionIndex) {
+          const questCopy = quest;
+
+          questCopy.isSaved = true;
+
+          return questCopy;
+        }
+
+        return quest;
+      })
+
+      return {
+        ...state,
+        questions
+      }
+    }
+    case "SET_TITLE": {
+      return {
+        ...state,
+        title: action.payload,
+      };
+    }
+    case "ADD_ANSWER": {
+      const questions = state.questions.map(quest => {
+        if (quest.id === action.payload.questionIndex) {
+          const questCopy = quest;
+
+          questCopy.answers[action.payload.answerId] = action.payload.currentAnswer;
+
+          return questCopy;
+        }
+
+        return quest;
+      })
+
+      return {
+        ...state,
+        questions
+      }
+    }
+    case "ADD_BLANK_ANSWER": {
+      const questions = state.questions.map(quest => {
+        if (quest.id === action.payload.questionIndex) {
+          const questCopy = quest;
+          const isAnswerLast = questCopy.answers.length - 1 === action.payload.answerId;
+
+          if (isAnswerLast) {
+            questCopy.answers = [...questCopy.answers, ''];
+          }
+
+          return questCopy;
+        }
+
+        return quest;
+      })
+
+      return {
+        ...state,
+        questions
+      }
+    }
+    case "ADD_CORRECT_ANSWER": {
+      const questions = state.questions.map(quest => {
+        if (quest.id === action.payload.questionIndex) {
+          const questCopy = quest;
+          const answerIndex = questCopy.answers.findIndex((ans) => ans === action.payload.currentAnswer);
+          
+          questCopy.correctAnswers = [...questCopy.correctAnswers, answerIndex];
+
+          return questCopy;
+        }
+
+        return quest;
+      })
+
+      return {
+        ...state,
+        questions
+      }
+    }
+    case "REMOVE_CORRECT_ANSWER": {
+      const questions = state.questions.map(quest => {
+        if (quest.id === action.payload.questionIndex) {
+          const questCopy = quest;
+          const answerIndex = questCopy.answers.findIndex((ans) => ans === action.payload.currentAnswer);
+          
+          questCopy.correctAnswers = questCopy.correctAnswers.filter((i) => i !== answerIndex);
+
+          return questCopy;
+        }
+
+        return quest;
+      })
+
+      return {
+        ...state,
+        questions
+      }
     }
     default:
       return state;
@@ -27,5 +159,6 @@ export const ConstructorContext = createContext();
 
 export const ConstructorProvider = ({children}) => {
   const value = useReducer(reducer, initialState);
+
   return <ConstructorContext.Provider value={value}>{children}</ConstructorContext.Provider>
 }
