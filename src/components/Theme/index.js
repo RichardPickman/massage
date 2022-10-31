14e26597d869f1cf2426b3ff2b6c21e6301f6150
RichardPickman <drezzerock@gmail.com>
2022-10-31 20:31:59 +0100

    feat: add theme compontnt

---
+++ src/components/Theme/index.js
@@ -0,0 +1,15 @@
+import { useContext } from "react"
+import { ThemeContext } from "../ThemeProvider"
+import { ThemeSwitch } from "./helpers"
+
+const Theme = () => {
+  const toggleTheme = useContext(ThemeContext);
+
+  const handleSwitch = () => toggleTheme.toggleColorMode();
+
+  return (
+    <ThemeSwitch checked={toggleTheme.darkTheme} onChange={handleSwitch} />
+  )
+}
+
+export default Theme;
