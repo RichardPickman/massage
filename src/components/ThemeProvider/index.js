2a1c2cc447829318d1d61e9c69c96ab11d62e6de
RichardPickman <drezzerock@gmail.com>
2022-10-31 20:32:07 +0100

    feat: add theme provider

---
+++ src/components/ThemeProvider/index.js
@@ -0,0 +1,31 @@
+import React, { useMemo, useState } from "react";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+
+export const ThemeContext = React.createContext({
+  toggleColorMode: () => {},
+  mode: "light"
+});
+
+const ThemeProviderHook = ({ children }) => {
+  const [darkEnabled, setDarkEnabled] = useState(false);
+  const colorMode = useMemo(() => ({
+    toggleColorMode: () => setDarkEnabled((prevMode) => (prevMode ? false : true)),
+    darkEnabled,
+  }), [darkEnabled]);
+
+  const dark = createTheme({ palette: { mode: 'dark' }});
+  const light = createTheme({ palette: { mode: 'light' }});
+
+  const mode = darkEnabled ? dark : light;
+
+  return (
+    <ThemeContext.Provider value={colorMode}>
+        <ThemeProvider theme={mode}>
+          <CssBaseline />
+          { children }
+        </ThemeProvider>
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeProviderHook;
