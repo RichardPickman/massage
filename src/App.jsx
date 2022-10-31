3f9c66fd82f5941107b0875076afb24cca447eee
RichardPickman <drezzerock@gmail.com>
2022-10-31 20:35:33 +0100

    feat: add main routes && header

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,36 @@
-import { ThemeProvider, createTheme, styled, Switch, Container } from "@mui/material"; 
-import CssBaseline from "@mui/material/CssBaseline";
-import { useState } from "react";
-import { MaterialUISwitch } from "./style";
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import QuizConstructor from "./pages/QuizConstructor";
-import './styles.css'
-
-const darkTheme = createTheme({ palette: { mode: 'dark' }});
-const lightTheme = createTheme({ palette: { mode: 'light' }});
+import Header from "./components/Header";
+import ThemeProviderHook from "./components/ThemeProvider";
+import Layout from "./components/Layout";
+import { AppContextProvider } from "./components/AppContextProvider";
+import './styles.css';
+import Login from "./pages/Login";
+import Quizzes from "./pages/Quizzes";
 
 const App = () => {
-  const getStoredTheme = Boolean(localStorage.getItem('theme'));
-  const [checked, setChecked] = useState(getStoredTheme);
-
-  const handleChange = (event) => {
-    localStorage.setItem('theme', event.target.checked);
-    setChecked(event.target.checked)
-  }
-
   return (
-    <ThemeProvider theme={checked ? darkTheme : lightTheme}>
-      <CssBaseline />
-      <header className="header">
-        <ul className="header__list">
-          <li className="header__link">Quizzes</li>
-          <li className="header__link">Quiz Constructor</li>
-          <li className="header__link"><MaterialUISwitch checked={checked} onChange={handleChange} /></li>
-        </ul>
-      </header>
-      <main>
-        <Container maxWidth="md">
-          <QuizConstructor />
-        </Container>
-        {/* <Container maxWidth="md">
-          <Quiz />
-        </Container> */}
-      </main>
-    </ThemeProvider>
+    <ThemeProviderHook>
+      <AppContextProvider>
+        <Header />
+        <Layout>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="quizzes" element={<Quizzes />}>
+              <Route path=":id" element={<Quiz />} />
+            </Route>
+            <Route path="lectures" element={<Quizzes />} />
+            <Route path="constructor" element={<QuizConstructor />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<QuizConstructor />} />
+            <Route path="profile" element={<QuizConstructor />} />
+          </Routes>
+        </Layout>
+      </AppContextProvider>
+    </ThemeProviderHook>
+    
   )
 }
 
