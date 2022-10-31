8a8ba584aa322e8b6e196592e6bc9270acaa555b
RichardPickman <drezzerock@gmail.com>
2022-10-31 20:32:32 +0100

    feat: add fetches

---
+++ src/http/quizApi.js
@@ -0,0 +1,33 @@
+import { $authHost, $host } from "./index";
+
+export const create = async (questions, title) => {
+  const { data } = await $host.post('api/quiz/create', { questions, title })
+
+  return data;
+}
+
+export const update = async (id, questions, title) => {
+  const { data } = await $host.post('api/quiz/update/:id', { id, questions, title })
+
+  return data;
+}
+
+export const get = async (id) => {
+  const { data } = await $host.post('api/quiz/:id', { id })
+
+  return data;
+}
+
+export const getAll = async () => {
+  const { data } = await $host.post('api/quiz/all');
+
+  console.log(data)
+
+  return data;
+}
+
+export const remove = async () => {
+  const { data } = await $authHost.get('api/quiz/remove/' )
+
+  return data;
+}
