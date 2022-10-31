f6da3a00496c3f6c27c7463f0ed5270d77783172
RichardPickman <drezzerock@gmail.com>
2022-10-31 20:31:45 +0100

    feat: add layout

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import { Box, Container } from '@mui/material';
+import React from 'react';
+
+
+const Layout = ({ children }) => {
+  return (
+    <Container maxWidth="sm">
+      {children}
+    </Container>
+  )
+}
+
+export default Layout;
