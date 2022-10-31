94a42b7ab48567e3c085d59ff1ad6ddbd5edce4f
RichardPickman <drezzerock@gmail.com>
2022-10-31 20:33:05 +0100

    refactor: add width

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -17,20 +17,20 @@ const Quiz = () => {
   }
 
   return (
-      <Box display="flex" flexDirection="column" alignItems="center" gap="1rem" >
-        <Typography variant="body1">
-          Question {quizState.currentQuestionIndex + 1} / {quizState.questions.length}
-        </Typography>
-        <Question />
-        <Stack direction="row" spacing={1}>
-          <Button variant="contained" disabled={buttonDisabled} onClick={prevQuestion}>
-            Previous question
-          </Button>
-          <Button variant="contained" onClick={nextQuestion}>
-            Next question
-          </Button>
-        </Stack>
-      </Box>
+    <Box display="flex" sx={{ width: '100%' }} flexDirection="column" alignItems="center" gap="1rem" >
+      <Typography variant="body1">
+        Question {quizState.currentQuestionIndex + 1} / {quizState.questions.length}
+      </Typography>
+      <Question />
+      <Stack direction="row" spacing={1}>
+        <Button variant="contained" disabled={buttonDisabled} onClick={prevQuestion}>
+          Previous question
+        </Button>
+        <Button variant="contained" onClick={nextQuestion}>
+          Next question
+        </Button>
+      </Stack>
+    </Box>
   )
 }
 
