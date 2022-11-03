acd6160a5b7a0a9269eeb1d5d49b2b72c3bd602b
RichardPickman <drezzerock@gmail.com>
2022-11-03 12:57:19 +0100

    feat: add server fetch

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -1,13 +1,31 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { QuizContext } from "../../context/quiz";
 import Question from "../../components/Question";
 import Result from "../../components/Result";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, Stack, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { get } from "../../http/quizApi";
+import ShowAnswers from "../../components/ShowAnswer";
 
 
 const Quiz = () => {
+  const { id } = useParams();
   const [quizState, dispatch] = useContext(QuizContext);
+  const [showAnswers, setShowAnswers] = useState(false);
   const buttonDisabled = quizState.currentQuestionIndex === 0;
+
+  const fetchQuiz = async () => {
+    try {
+      const data = await get(id);
+      // console.log("Fetching data...");
+      dispatch({ type: "SET_QUIZ", payload: data.payload });
+      // console.log("Server response: ", data);
+    } catch(e) {
+      alert(e.message);
+    }
+  }
+
+  useEffect(() => {fetchQuiz()}, []);
 
   const nextQuestion = () => dispatch({ type: 'NEXT_QUESTION' });
   const prevQuestion = () => dispatch({ type: 'PREV_QUESTION' });
@@ -17,19 +35,25 @@ const Quiz = () => {
   }
 
   return (
-    <Box display="flex" sx={{ width: '100%' }} flexDirection="column" alignItems="center" gap="1rem" >
-      <Typography variant="body1">
-        Question {quizState.currentQuestionIndex + 1} / {quizState.questions.length}
-      </Typography>
-      <Question />
-      <Stack direction="row" spacing={1}>
-        <Button variant="contained" disabled={buttonDisabled} onClick={prevQuestion}>
-          Previous question
-        </Button>
-        <Button variant="contained" onClick={nextQuestion}>
-          Next question
-        </Button>
-      </Stack>
+    <Box display="flex" sx={{ width: '100%' }} flexDirection="column" alignItems="center" justifyContent="center" gap="1rem" >
+      {quizState.questions.length === 0 ? <CircularProgress /> : (
+        <>
+          <Typography variant="body1">
+            {quizState.title}
+          </Typography>
+          <Typography variant="body1">
+            Question {quizState.currentQuestionIndex + 1} / {quizState.questions.length}
+          </Typography>
+          <Question showAnswers={showAnswers} />
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained" disabled={buttonDisabled} onClick={prevQuestion}>
+              Previous question
+            </Button>
+            <ShowAnswers showAnswersState={showAnswers} onChange={(state) => setShowAnswers(state)} />
+            <Button variant="contained" onClick={nextQuestion}>Next question</Button>
+          </Stack>
+        </>
+      )}
     </Box>
   )
 }
