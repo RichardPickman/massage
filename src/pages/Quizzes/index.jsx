c6a396d20c9e6ecd811705c5c6cdae1d5e129973
RichardPickman <drezzerock@gmail.com>
2022-11-03 12:58:07 +0100

    refactor: change link appearance

--- src/pages/Quizzes/index.jsx
+++ src/pages/Quizzes/index.jsx
@@ -18,21 +18,22 @@ const Quizzes = () => {
     }
 
     fetchQuizzes();
-  }, [])
+  }, []);
 
   return (
     <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" gap={1} margin={2}>
       <Typography variant="h3">Quizzes</Typography>
       <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-        {quizzes.map((quiz, index) => {
-          return (
-            <Button key={index}>
-              <Link underline="none" to={quiz.id} components={RouterLink}>
-                {quiz.title}
-              </Link>
-            </Button>
-          )
-        })}
+        {quizzes.map((quiz, index) => (
+          <Link 
+            underline="none"
+            key={index} 
+            to={`/quiz/${quiz._id}`} 
+            component={RouterLink}
+          >
+            <Button>{quiz.title}</Button>
+          </Link>
+        ))}
       </Box>
     </Box>
   )
