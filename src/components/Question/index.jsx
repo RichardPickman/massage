fc4acd6180f93f060bd4a3e9eee161b08d37903b
RichardPickman <drezzerock@gmail.com>
2022-11-03 12:54:41 +0100

    refactor: change answer props

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,34 +1,35 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Card, Stack, Typography } from "@mui/material";
 import { useContext } from "react";
 import { QuizContext } from "../../context/quiz";
 import Answer from "../Answer";
 import "./style.css";
 
 
-const Question = () => {
+const Question = ({ showAnswers }) => {
   const [quizState, dispatch] = useContext(QuizContext);
   const currentQuestion = quizState.questions[quizState.currentQuestionIndex];
 
   const answerProps = (answer, index) => ({
     answerText: answer,
-    answers: currentQuestion.answers,
+    isFinished: quizState.showResults,
+    showAnswers,
     index,
-    key: index, 
-    currentAnswer: quizState.currentAnswers,
-    correctAnswer: currentQuestion.correctAnswer,
-    onSelectAnswer: (answerText) => dispatch({ type: 'SELECT_ANSWER', payload: answerText })
-  })
+    answers: currentQuestion.answers,
+    currentAnswers: quizState.currentAnswers,
+    correctAnswers: currentQuestion.correctAnswers,
+    onSelectAnswer: (answerIndex) => dispatch({ type: 'SELECT_ANSWER', payload: { answerIndex } })
+  });
 
   return (
-    <Box  display="flex" flexDirection="column" alignItems="center" gap="1rem">
-      {currentQuestion.question && <Typography variant="body1">{currentQuestion.question}</Typography>}
-      <Box display="flex" justifyContent="center">
-        <img className="image" src={currentQuestion.img} alt="" />
+    <Card>
+      <Box  display="flex" flexDirection="column" alignItems="center" gap="1rem" margin={2}>
+        {currentQuestion.question && <Typography variant="body1">{currentQuestion.question}</Typography>}
+        {currentQuestion.img &&  <Box display="flex" justifyContent="center"><img className="image" src={currentQuestion.img} alt="" /></Box>}
+        <Stack direction="row" spacing={1}>
+          {currentQuestion.answers.map((answer, index) => (<Answer key={index} answerProps={answerProps(answer, index)} />))}
+        </Stack>
       </Box>
-      <Stack direction="row" spacing={1}>
-        {quizState.answers.map((answer, index) => (<Answer key={index} answerProps={answerProps(answer, index)} />))}
-      </Stack>
-    </Box>
+    </Card>
   )
 }
 
