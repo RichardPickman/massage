bcee6608f3a552226bd6759aefef9f43f903cd33
RichardPickman <drezzerock@gmail.com>
2022-11-03 12:57:52 +0100

    feat: redesign to props oriented constructor

--- src/pages/QuizConstructor/index.jsx
+++ src/pages/QuizConstructor/index.jsx
@@ -1,5 +1,5 @@
-import { useContext, useEffect, useState } from "react";
-import { Stack, TextField, Button, Box } from "@mui/material";
+import { memo, useContext, useEffect, useState } from "react";
+import { Stack, TextField, Button, Box, CircularProgress } from "@mui/material";
 import { ConstructorContext } from "../../context/quizConstructor";
 import QuestionTemplate from "./template";
 
@@ -8,61 +8,92 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 import { create } from "../../http/quizApi";
 
+const questionTemp = {
+  question: "",
+  img: "",
+  answers: [''],
+  correctAnswers: [],
+  isSaved: false,
+}
+
 const QuizConstructor  = () => {
-  const [constructorState, dispatch] = useContext(ConstructorContext);
+  const [questionsLoading, setQuestionsLoading] = useState(0);
+  const [questions, setQuestions] = useState([]);
   
   const [title, setTitle] = useState('');
 
-  const setQuestionsCount = (count) => {
-    for (let i=0; i < count; i++) {
-      dispatch({  type: 'ADD_QUESTION' })
+  const setQuestionsCount = async (amount) => {
+    setQuestionsLoading(true);
+
+    const result = [];
+
+    for (let i=0; i < amount; i++) {
+      const temp = { ...questionTemp, id: i };
+
+      result.push(temp);
     }
+
+    setQuestions(result);
+
+    setQuestionsLoading(false);
   };
 
-  useEffect(() => dispatch({ type: 'SET_TITLE', payload: title }), [title, dispatch]);
+  const updateQuestion = (questionId, data) => {
+    setQuestions(currQuestions => {
+      return currQuestions.map((question) => {
+        if (questionId === question._id) {
+          return {
+            ...questions, data
+          }
+        }
 
-  const saveQuiz = () => create(constructorState.questions, constructorState.title);
+        return question;
+      })
+    })
+  }
 
-  return (
-      <Box display="flex" flexDirection="column" justifyContent="center" gap="2rem">
-        <Box display="flex" flexDirection="column" justifyContent="center" gap="1rem">
-          <Stack direction="column" spacing={2}>
-            <TextField 
-            id="outlined-basic" 
-            label="Quiz title" 
-            variant="outlined" 
-            onChange={(event) => setTitle(event.target.value)} 
-            value={title}
-            />
-            <TextField 
-            id="outlined-basic" 
-            label="Questions amount" 
-            variant="outlined" 
-            type="number"
-            onChange={(event) => setQuestionsCount(event.target.value)}
-            />
-          </Stack>
-        </Box>
-        <Box display="flex" flexDirection="column" justifyContent="center" gap="1rem">
-          {constructorState.questions.length > 0  && 
-          constructorState.questions.map((question, index) => <QuestionTemplate key={index} questionIndex={index} />)}
-        </Box>
-        <Button
-            variant="contained"
-            color="success"
-            onClick={saveQuiz}
-            endIcon={<SaveIcon />}>
-            Save quiz
-          </Button>
-          <Button
-            variant="contained"
-            color="error"
-            onClick={saveQuiz}
-            endIcon={<DeleteForeverIcon />}>
-            Delete quiz
-          </Button>
-      </Box>
-  );
+  const saveQuiz = () => create(questions, title);
+
+  return questionsLoading ? 
+    <CircularProgress /> : 
+    <Box display="flex" flexDirection="column" justifyContent="center" gap="2rem">
+    <Box display="flex" flexDirection="column" justifyContent="center" gap="1rem">
+      <Stack direction="column" spacing={2}>
+        <TextField 
+        id="outlined-basic" 
+        label="Quiz title" 
+        variant="outlined" 
+        onChange={(event) => setTitle(event.target.value)} 
+        value={title}
+        />
+        <TextField 
+        id="outlined-basic" 
+        label="Questions amount" 
+        variant="outlined" 
+        type="number"
+        inputProps={{ min: 1, max: 100 }}
+        onChange={(event) => setQuestionsCount(event.target.value)}
+        />
+      </Stack>
+    </Box>
+    <Box display="flex" flexDirection="column" justifyContent="center" gap="1rem">
+      {questions.map((item, index) => <QuestionTemplate key={item.id} questions={questions} updateQuestion={updateQuestion} questionId={index} />)}
+    </Box>
+    <Button
+        variant="contained"
+        color="success"
+        onClick={saveQuiz}
+        endIcon={<SaveIcon />}>
+        Save quiz
+      </Button>
+      <Button
+        variant="contained"
+        color="error"
+        onClick={saveQuiz}
+        endIcon={<DeleteForeverIcon />}>
+        Delete quiz
+      </Button>
+  </Box>
 }
 
 export default QuizConstructor;
--- src/pages/QuizConstructor/template.jsx
+++ src/pages/QuizConstructor/template.jsx
@@ -1,50 +1,88 @@
 import { Card, TextField, Button, Box, Grid } from "@mui/material";
-import { useContext, useState } from "react";
+import { useEffect, useState } from "react";
 
-import SaveIcon from '@mui/icons-material/Save';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+
 import AnswerWithCheckbox from "../../components/AnswerWithCheckbox";
-import { ConstructorContext } from "../../context/quizConstructor";
 import { memo } from "react";
 
-const QuestionTemplate = ({ questionIndex }) => {
-  const [constructorState, dispatch] = useContext(ConstructorContext);
-  const [image, setImage] = useState(null);
-  const [question, setQuestion] = useState('');
+const QuestionTemplate = ({ questionId, questions, updateQuestion }) => {
+  const [questionState, setQuestionState] = useState(({
+    id: questionId,
+    question: "",
+    img: "",
+    answers: [''],
+    correctAnswers: [],
+    isSaved: false,
+  }))
 
-  const currentQuestion  = constructorState.questions[questionIndex];
+  const setTitle = (text) => setQuestionState({ ...questionState, question: text });
 
-  const uploadImage = (event) => {
-    let img = event.target.files[0];
+  const addAnswer = (answer, answerId) => {
+    const answers = [...questionState.answers];
 
-    setImage(URL.createObjectURL(img));
+    answers[answerId] = answer;
+
+    setQuestionState({
+      ...questionState, 
+      answers,
+    })
   }
 
-  const saveQuestion = () => dispatch({ type: "SAVE_QUESTION", payload: { questionIndex } });
+  const handleCorrectAnswer = (index) => {
+    const isExist = questionState.correctAnswers.includes(index);
+    const correctAnswers = isExist ? 
+    questionState.correctAnswers.filter(item => item !== index) : 
+    [...questionState.correctAnswers, index];
+
+    return setQuestionState({
+      ...questionState,
+      correctAnswers,
+    })
+  };
+
+  const addBlank = (index) => index === questionState.answers.length - 1 && 
+  setQuestionState({
+    ...questionState, 
+    answers: [...questionState.answers, '']
+  });
+
+  useEffect(() => updateQuestion(questionId, questionState), [questionState])
 
   return (
     <Card variant="outlined" sx={{ width: '100%' }}>
       <Box display="flex" margin="1rem" flexDirection="column" justifyContent="center" gap="1rem" direction="row" spacing={2}>
-        <TextField id="outlined-basic" label="Question" variant="outlined" onChange={(event) => setQuestion(event.target.value)} />
-        <Button variant="contained" component="label">
+        <TextField id="outlined-basic" label="Question" variant="outlined" onChange={(event) => setTitle(event.target.value)} />
+        {/* <Button variant="contained" component="label">
           Upload
           <input hidden accept="image/*" type="file" onChange={uploadImage} />
-        </Button>
+        </Button> */}
         <Grid container spacing={2}>
-          {currentQuestion.answers.map((answer, i) => (<Grid item key={i} xs={6}><AnswerWithCheckbox questionIndex={questionIndex} answerId={i} /></Grid>))}
+          {questionState.answers.map((answer, i) => (
+            <Grid item key={i} xs={6}>
+              <AnswerWithCheckbox 
+                questionId={questionId} 
+                answerId={i}
+                addAnswer={addAnswer} 
+                handleCorrectAnswer={handleCorrectAnswer} 
+                addBlank={addBlank}  
+              />
+            </Grid>
+          ))}
         </Grid>
         <Box display="flex" justifyContent="right" gap="1rem" marginTop="1rem">
           <Button
             variant="contained"
             color="success"
-            onClick={saveQuestion}
-            endIcon={<SaveIcon />}>
+            // onClick={saveQuestion}
+            endIcon={<SaveOutlinedIcon />}>
             Save
           </Button>
           <Button
             variant="contained"
             color="error"
-            endIcon={<DeleteForeverIcon />}>
+            endIcon={<DeleteOutlineOutlinedIcon />}>
             Delete
           </Button>
         </Box>
