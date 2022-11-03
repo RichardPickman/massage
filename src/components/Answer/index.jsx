22d81325aea228d960a74642bd2f5b98598a1976
RichardPickman <drezzerock@gmail.com>
2022-11-03 12:53:43 +0100

    refactor: answer revisited

--- src/components/Answer/index.jsx
+++ src/components/Answer/index.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button } from '@mui/material';
 import './style.css';
 
 const Answer = ({ answerProps }) => {
@@ -6,25 +6,49 @@ const Answer = ({ answerProps }) => {
     answerText, 
     index, 
     onSelectAnswer,
-    currentAnswer,
-    correctAnswer 
+    currentAnswers,
+    correctAnswers,
+    showAnswers,
+    isFinished,
   } = answerProps;
 
-  const { innerWidth } = window;
+  const answered = currentAnswers.includes(index) ? "contained" : "outlined";
 
-  const letterMapping = ['A', 'B', 'C', 'D'];
-  const isCorrectAnswer = currentAnswer && answerText === correctAnswer;
-  const isWrongAnswer = currentAnswer === answerText && currentAnswer !== correctAnswer;
-  const wrongClass = isWrongAnswer ? "wrong" : "";
-  const correctClass = isCorrectAnswer ? "success" : "";
-  const disabledClass = currentAnswer ? "disabled" : "";
+  if (showAnswers) {
+    if (correctAnswers.includes(index)) {
+      return <Button variant="outlined" color="success">{answerText}</Button>
+    }
+    if (!correctAnswers.includes(index)) {
+      return <Button variant="outlined" disabled>{answerText}</Button>
+    }
+  }
+
+  if (isFinished) {
+    if (correctAnswers.includes(index)) {
+      return currentAnswers.includes(index) ?  
+      <Button variant="contained" color="success">{answerText}</Button> :
+      <Button variant="outlined" color="success">{answerText}</Button>
+    }
+    if (!correctAnswers.includes(index) && currentAnswers.includes(index)) {
+      return currentAnswers.includes(index) ?  
+      <Button variant="contained" color="error">{answerText}</Button> :
+      <Button variant="outlined" color="error">{answerText}</Button>
+    }
+
+    if (!currentAnswers.includes(index)) {
+      return <Button variant="outlined" disabled>{answerText}</Button>
+    }
+
+  }
+
   return (
-    <div
-    className={`answer ${wrongClass} ${correctClass} ${disabledClass}`}
-    onClick={() => onSelectAnswer(answerText)}>
-      {innerWidth > 840 && `${letterMapping[index]}.`} {answerText}
-    </div>
-  )
+    <Button 
+      variant={answered} 
+      onClick={() => onSelectAnswer(index)}
+    >
+      {answerText}
+    </Button>
+  );  
 }
 
 export default  Answer;
