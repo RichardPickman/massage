21943ff49fa84d05de08ef2a0640e1de717e79a8
RichardPickman <drezzerock@gmail.com>
2022-11-04 19:45:38 +0100

    feat: component re-designed for reusability

--- src/components/AnswerWithCheckbox/index.jsx
+++ src/components/AnswerWithCheckbox/index.jsx
@@ -1,16 +1,40 @@
 import { useRef } from "react";
 import { TextField, Checkbox, FormControlLabel, FormControl } from "@mui/material";
+import { useCallback, useState } from "react";
+import { useEffect } from "react";
 
 const AnswerWithCheckbox = (props) => {
-  const { answerId,addAnswer,handleCorrectAnswer,addBlank } = props;
+  const { 
+    answerId,
+    addAnswer,
+    handleCorrectAnswer,
+    addBlank,
+    predefinedText,
+    predefinedCheckbox 
+  } = props;
+  const [text, setText] = useState(predefinedText)
+  const [checked, setChecked] = useState(predefinedCheckbox);
   
-  const correct = useRef();
-  const answer = useRef();
+  // const onBlur = () => useCallback(() => addAnswer(text, answerId), [text]);
+  // const onChange = () => useCallback(() => addBlank(answerId), [checked]);
+  // const handleCorrect = () => useCallback(() => handleCorrectAnswer(answerId), [checked]);
+
+  useEffect(() => addAnswer(text, answerId), [text])
+
+  const onChange = (text) => {
+    setText(text.target.value);
+    addBlank(answerId);
+  }
+
+  const handleCorrect = () => {
+    setChecked(checked);
+    handleCorrectAnswer(answerId);
+  }
 
   return (
-    <FormControl>
-      <TextField variant="filled" onBlur={() => addAnswer(answer.current.value, answerId)} onChange={() => addBlank(answerId)} inputRef={answer}></TextField>
-      <FormControlLabel control={<Checkbox inputRef={correct} onChange={() => handleCorrectAnswer(answerId)} />} label="Correct answer" />
+    <FormControl fullWidth>
+      <TextField value={predefinedText} variant="filled" onChange={onChange}></TextField>
+      <FormControlLabel control={<Checkbox checked={predefinedCheckbox} onChange={handleCorrect} />} label="Correct answer" />
     </FormControl>
   )
 }
