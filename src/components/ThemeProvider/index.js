81c4a38f3f3994e7d3b9cc4370c592c4595864ca
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:52:10 +0100

    refactor: code style change

--- src/components/ThemeProvider/index.js
+++ src/components/ThemeProvider/index.js
@@ -1,31 +1,40 @@
-import React, { useMemo, useState } from "react";
-import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+import React, { useMemo, useState } from 'react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import PropTypes from 'prop-types';
 
 export const ThemeContext = React.createContext({
-  toggleColorMode: () => {},
-  mode: "light"
+    toggleColorMode: () => {},
+    mode: 'light',
 });
 
 const ThemeProviderHook = ({ children }) => {
-  const [darkEnabled, setDarkEnabled] = useState(false);
-  const colorMode = useMemo(() => ({
-    toggleColorMode: () => setDarkEnabled((prevMode) => (prevMode ? false : true)),
-    darkEnabled,
-  }), [darkEnabled]);
+    const [darkEnabled, setDarkEnabled] = useState(false);
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () =>
+                setDarkEnabled((prevMode) => (prevMode ? false : true)),
+            darkEnabled,
+        }),
+        [darkEnabled]
+    );
 
-  const dark = createTheme({ palette: { mode: 'dark' }});
-  const light = createTheme({ palette: { mode: 'light' }});
+    const dark = createTheme({ palette: { mode: 'dark' } });
+    const light = createTheme({ palette: { mode: 'light' } });
 
-  const mode = darkEnabled ? dark : light;
+    const mode = darkEnabled ? dark : light;
 
-  return (
-    <ThemeContext.Provider value={colorMode}>
-        <ThemeProvider theme={mode}>
-          <CssBaseline />
-          { children }
-        </ThemeProvider>
-    </ThemeContext.Provider>
-  )
-}
+    return (
+        <ThemeContext.Provider value={colorMode}>
+            <ThemeProvider theme={mode}>
+                <CssBaseline />
+                {children}
+            </ThemeProvider>
+        </ThemeContext.Provider>
+    );
+};
 
 export default ThemeProviderHook;
+
+ThemeProviderHook.propTypes = {
+    children: PropTypes.node,
+};
