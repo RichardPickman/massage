d245fb72a2ca3037b1d9da5f41ffc44617dc6b8e
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:58:21 +0100

    refactor: tabs and quotes

--- src/pages/Quizzes/index.jsx
+++ src/pages/Quizzes/index.jsx
@@ -1,42 +1,53 @@
-import { Box, Button, Link, Typography } from "@mui/material"
-import { Link as RouterLink } from "react-router-dom";
-import { useEffect, useState } from "react"
-import { getAll } from "../../http/quizApi";
-
+import { Box, Button, Link, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { getAllQuizzes } from '../../http/quizApi';
 
 const Quizzes = () => {
-  const [quizzes, setQuizzes] = useState([]);
+    const [quizzes, setQuizzes] = useState([]);
 
-  useEffect(() => {
-    async function fetchQuizzes() {
-      try {
-        const data = await getAll();
-        setQuizzes([...data.payload]);
-      } catch(e) {
-        alert(e.message)
-      }
-    }
+    useEffect(() => {
+        async function fetchQuizzes() {
+            try {
+                const { data } = await getAllQuizzes();
+                setQuizzes([...data.payload]);
+            } catch (e) {
+                alert(e.message);
+            }
+        }
 
-    fetchQuizzes();
-  }, []);
+        fetchQuizzes();
+    }, []);
 
-  return (
-    <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" gap={1} margin={2}>
-      <Typography variant="h3">Quizzes</Typography>
-      <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-        {quizzes.map((quiz, index) => (
-          <Link 
-            underline="none"
-            key={index} 
-            to={`/quiz/${quiz._id}`} 
-            component={RouterLink}
-          >
-            <Button>{quiz.title}</Button>
-          </Link>
-        ))}
-      </Box>
-    </Box>
-  )
-}
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            gap={1}
+            margin={2}
+        >
+            <Typography variant="h3">Quizzes</Typography>
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+                {quizzes.map((quiz, index) => (
+                    <Link
+                        underline="none"
+                        key={index}
+                        to={`/quizzes/${quiz._id}`}
+                        component={RouterLink}
+                    >
+                        <Button>{quiz.title}</Button>
+                    </Link>
+                ))}
+            </Box>
+        </Box>
+    );
+};
 
 export default Quizzes;
