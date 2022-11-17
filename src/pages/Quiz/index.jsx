59fa077cd1023fd6016757ae54339b4ea3e0e1f5
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:56:32 +0100

    feat: update styles

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -1,63 +1,121 @@
-import { useContext, useEffect, useState } from "react";
-import { QuizContext } from "../../context/quiz";
-import Question from "../../components/Question";
-import Result from "../../components/Result";
-import { Box, Button, CircularProgress, Stack, Typography } from "@mui/material";
-import { useParams } from "react-router-dom";
-import { get } from "../../http/quizApi";
-import ShowAnswers from "../../components/ShowAnswer";
-import { memo } from "react";
-import { useCallback } from "react";
-
+import React, { useContext, useEffect, useState } from 'react';
+import { QuizContext } from '../../context/quiz';
+import Question from '../../components/Question';
+import Result from '../../components/Result';
+import {
+    Box,
+    Button,
+    Card,
+    CircularProgress,
+    Stack,
+    Typography,
+    CardHeader,
+    CardActions,
+    CardContent,
+} from '@mui/material';
+import { useParams } from 'react-router-dom';
+import { getQuiz } from '../../http/quizApi';
+import ShowAnswers from '../../components/ShowAnswer';
+import { useCallback } from 'react';
 
 const Quiz = () => {
-  const { id } = useParams();
-  const [quizState, dispatch] = useContext(QuizContext);
-  const [showAnswers, setShowAnswers] = useState(false);
-  const buttonDisabled = quizState.currentQuestionIndex === 0;
+    const { id } = useParams();
+    const [quizState, dispatch] = useContext(QuizContext);
+    const [showAnswers, setShowAnswers] = useState(false);
+    const buttonDisabled = quizState.currentQuestionIndex === 0;
+    const currentQuestion = quizState.questions[quizState.currentQuestionIndex];
 
-  const fetchQuiz = async () => {
-    try {
-      const data = await get(id);
-      // console.log("Fetching data...");
-      dispatch({ type: "SET_QUIZ", payload: data.payload });
-      // console.log("Server response: ", data);
-    } catch(e) {
-      alert(e.message);
+    useEffect(() => {
+        const fetchQuiz = async () => {
+            try {
+                const { data } = await getQuiz(id);
+                dispatch({ type: 'SET_QUIZ', payload: data.payload });
+            } catch (e) {
+                console.log(e.message);
+            }
+        };
+
+        fetchQuiz();
+    }, []);
+
+    const nextQuestion = useCallback(
+        () => dispatch({ type: 'NEXT_QUESTION' }),
+        [dispatch]
+    );
+    const prevQuestion = useCallback(
+        () => dispatch({ type: 'PREV_QUESTION' }),
+        [dispatch]
+    );
+
+    const toggleQuestion = useCallback((state) => setShowAnswers(state), []);
+
+    if (quizState.showResults) {
+        return <Result />;
     }
-  }
 
-  useEffect(() => {fetchQuiz()}, []);
-
-  const nextQuestion = useCallback(() => dispatch({ type: 'NEXT_QUESTION' }), [dispatch]);
-  const prevQuestion = useCallback(() => dispatch({ type: 'PREV_QUESTION' }), [dispatch]);
-
-  if (quizState.showResults) {
-    return <Result />
-  }
-
-  return (
-    <Box display="flex" sx={{ width: '100%' }} flexDirection="column" alignItems="center" justifyContent="center" gap="1rem" >
-      {quizState.questions.length === 0 ? <CircularProgress /> : (
-        <>
-          <Typography variant="body1">
-            {quizState.title}
-          </Typography>
-          <Typography variant="body1">
-            Question {quizState.currentQuestionIndex + 1} / {quizState.questions.length}
-          </Typography>
-          <Question showAnswers={showAnswers} />
-          <Stack direction="row" spacing={2}>
-            <Button variant="contained" disabled={buttonDisabled} onClick={prevQuestion}>
-              Previous question
-            </Button>
-            <ShowAnswers showAnswersState={showAnswers} onChange={(state) => setShowAnswers(state)} />
-            <Button variant="contained" onClick={nextQuestion}>Next question</Button>
-          </Stack>
-        </>
-      )}
-    </Box>
-  )
-}
+    return (
+        <Box
+            display="flex"
+            sx={{ width: '100%' }}
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            gap="1rem"
+        >
+            {quizState.questions.length === 0 ? (
+                <CircularProgress />
+            ) : (
+                <Card>
+                    <CardHeader
+                        title={quizState.title}
+                        subheader={`Question ${
+                            quizState.currentQuestionIndex + 1
+                        } / 
+                        ${quizState.questions.length}`}
+                    >
+                        <Typography variant="body1">{}</Typography>
+                        <Typography variant="body1"></Typography>
+                    </CardHeader>
+                    <CardContent>
+                        <Question
+                            currentState={{
+                                ...quizState,
+                                showAnswers: showAnswers,
+                                onSelectAnswer: (answerIndex) =>
+                                    dispatch({
+                                        type: 'SELECT_ANSWER',
+                                        payload: { answerIndex },
+                                    }),
+                            }}
+                            currentQuestion={currentQuestion}
+                        />
+                    </CardContent>
+                    <CardActions
+                        sx={{
+                            width: '100%',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Button
+                            variant="contained"
+                            disabled={buttonDisabled}
+                            onClick={prevQuestion}
+                        >
+                            Previous question
+                        </Button>
+                        <ShowAnswers
+                            showAnswersState={showAnswers}
+                            onChange={toggleQuestion}
+                        />
+                        <Button variant="contained" onClick={nextQuestion}>
+                            Next question
+                        </Button>
+                    </CardActions>
+                </Card>
+            )}
+        </Box>
+    );
+};
 
 export default Quiz;
