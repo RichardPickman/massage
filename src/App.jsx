1a8a5f51a4fc45372eb47e607d01c5c695ae288d
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:59:16 +0100

    feat: add new routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,63 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Quiz from "./pages/Quiz";
-import QuizConstructor from "./pages/QuizConstructor";
-import Header from "./components/Header";
-import ThemeProviderHook from "./components/ThemeProvider";
-import Layout from "./components/Layout";
-import { AppContextProvider } from "./components/AppContextProvider";
+import React from 'react';
+import Home from './pages/Home';
+import Quiz from './pages/Quiz';
+import Login from './pages/Login';
+import Quizzes from './pages/Quizzes';
+import Lecture from './pages/Lectures/Lecture';
+import Lectures from './pages/Lectures';
+import Header from './components/Header';
+import Layout from './components/Layout';
+import QuizConstructor from './pages/QuizConstructor';
+import ThemeProviderHook from './components/ThemeProvider';
+import { Route, Routes } from 'react-router-dom';
+import { AppContextProvider } from './components/AppContextProvider';
 import './styles.css';
-import Login from "./pages/Login";
-import Quizzes from "./pages/Quizzes";
+import AddLecture from './pages/Lectures/AddLecture';
+import { LoadingProvider } from './context/loading';
 
 const App = () => {
-  return (
-    <ThemeProviderHook>
-      <AppContextProvider>
-        <Header />
-        <Layout>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/quizzes" element={<Quizzes />} />
-            <Route path="/quiz">
-              <Route path=":id" element={<Quiz />} />
-            </Route>
-            <Route path="/lectures" element={<Quizzes />} />
-            <Route path="/constructor" element={<QuizConstructor />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<QuizConstructor />} />
-            <Route path="/profile" element={<QuizConstructor />} />
-          </Routes>
-        </Layout>
-      </AppContextProvider>
-    </ThemeProviderHook>
-    
-  )
-}
+    return (
+        <ThemeProviderHook>
+            <AppContextProvider>
+                <ThemeProviderHook>
+                    <Header />
+                    <Layout>
+                        <LoadingProvider>
+                            <Routes>
+                                <Route index element={<Home />} />
+                                <Route path="/quizzes">
+                                    <Route index element={<Quizzes />} />
+                                    <Route path=":id" element={<Quiz />} />
+                                    <Route
+                                        path="constructor"
+                                        element={<QuizConstructor />}
+                                    />
+                                </Route>
+                                <Route path="/lectures">
+                                    <Route index element={<Lectures />} />
+                                    <Route path=":id" element={<Lecture />} />
+                                    <Route
+                                        path="create"
+                                        element={<AddLecture />}
+                                    />
+                                </Route>
+
+                                <Route path="/login" element={<Login />} />
+                                <Route
+                                    path="/register"
+                                    element={<QuizConstructor />}
+                                />
+                                <Route
+                                    path="/profile"
+                                    element={<QuizConstructor />}
+                                />
+                            </Routes>
+                        </LoadingProvider>
+                    </Layout>
+                </ThemeProviderHook>
+            </AppContextProvider>
+        </ThemeProviderHook>
+    );
+};
 
 export default App;
