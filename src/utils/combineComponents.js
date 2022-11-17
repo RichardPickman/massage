c4b43d720cfa9c923ba782228983c278775c5d4c
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:59:00 +0100

    refactor: change consts to JS file and refactor combine

--- src/utils/combineComponents.js
+++ src/utils/combineComponents.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const combineComponents = (...components) => {
-  return components.reduce(
-    (AccumulatedComponents, CurrentComponent) => {
-      return ({ children }) => {
-        return (
-          <AccumulatedComponents>
-            <CurrentComponent>{children}</CurrentComponent>
-          </AccumulatedComponents>
-        );
-      };
-    },
-    ({ children }) => <>{children}</>,
-  );
+    return components.reduce(
+        (AccumulatedComponents, CurrentComponent) => {
+            return function wrapper({ children }) {
+                return (
+                    <AccumulatedComponents>
+                        <CurrentComponent>{children}</CurrentComponent>
+                    </AccumulatedComponents>
+                );
+            };
+        },
+        ({ children }) => <>{children}</>
+    );
+};
+
+combineComponents.propTypes = {
+    components: PropTypes.array,
 };
---
+++ src/utils/consts.js
@@ -0,0 +1,5 @@
+export const ADMIN_ROUTE = '/admin';
+export const LOGIN_ROUTE = '/login';
+export const REGISTRATION_ROUTE = '/registration';
+export const QUIZES_ROUTE = '/quiz';
+export const QUIZ_CONSTRUCTOR_ROUTE = '/quiz';
