dacbb36973cfd1d1b9de74e3afbcff8c09cc7952
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:49:51 +0100

    feat: update header component and created own context

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,75 +1,45 @@
-import { Link as RouterLink } from "react-router-dom";
-import { Box, Link } from "@mui/material";
-import Theme from '../Theme'
+import { Link as RouterLink } from 'react-router-dom';
+import { Box, Link } from '@mui/material';
+import Theme from '../Theme';
+import React from 'react';
+import { useContext } from 'react';
+import { LayoutContext } from '../../context/layout';
 
 const Header = () => {
-  return (
-    <header className="header">
-      <Box display="flex" justifyContent="space-around" alignItems="center">
-        <ul className="header__list">
-          <Box display="flex" justifyContent="center" alignItems="center" gap={4} margin={2}> 
-            <li className="header__link">
-              <Link to="/" component={RouterLink}>
-                Home
-              </Link>
-            </li>
-            <li className="header__link">
-              <Link to="/lectures" component={RouterLink} >
-                Lectures
-              </Link>
-            </li>
-            <li className="header__link">
-              <Link to="/quizzes" component={RouterLink} >
-                Quizzes
-              </Link>
-            </li>
-            <li className="header__link">
-              <Link to="/constructor" component={RouterLink} >
-                Constructor
-              </Link>
-            </li>
-            <li className="header__link">
-              <Theme />
-            </li>
-          </Box>
-        </ul>
-        {/* TODO: ADD ACCOUNTS */}
-        {/* {userState.isAuth && 
-          <ul className="header__list">
-          <Box display="flex" justifyContent="center" alignItems="center" gap={4} margin={2}> 
-            <li className="header__link">
-              <Link to="/login" component={RouterLink} >
-                <Avatar src="./img/no_avatar" />
-              </Link>
-            </li>
-            <li className="header__link">
-              <Theme />
-            </li>
-          </Box>
-        </ul>
-        }
-        {!userState.isAuth && 
-          <ul className="header__list">
-            <Box display="flex" justifyContent="center" alignItems="center" gap={4} margin={2}> 
-              <li className="header__link">
-                <Link to="/login" component={RouterLink} >
-                  Login
-                </Link>
-              </li>
-              <li className="header__link">
-                <Link to="/register" component={RouterLink} >
-                  Register
-                </Link>
-              </li>
-              <li className="header__link">
-                <Theme />
-              </li>
-            </Box>
-          </ul>
-        } */}
-      </Box>
-    </header>
-  )
-}
+    const [headerState] = useContext(LayoutContext);
+
+    return (
+        headerState.isShown && (
+            <header className="header">
+                <Box
+                    display="flex"
+                    justifyContent="space-around"
+                    alignItems="center"
+                >
+                    <ul className="header__list">
+                        <Box
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            gap={4}
+                            margin={2}
+                        >
+                            {headerState.links.map((link, index) => (
+                                <li key={index} className="header__link">
+                                    <Link to={link.path} component={RouterLink}>
+                                        {link.name}
+                                    </Link>
+                                </li>
+                            ))}
+                            <li className="header__link">
+                                <Theme />
+                            </li>
+                        </Box>
+                    </ul>
+                </Box>
+            </header>
+        )
+    );
+};
 
 export default Header;
---
+++ src/context/layout.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { createContext, useReducer } from 'react';
+import PropTypes from 'prop-types';
+
+const initialState = {
+    isShown: true,
+    layoutShown: true,
+    links: [
+        {
+            name: 'Home',
+            path: '/',
+        },
+        {
+            name: 'Lectures',
+            path: '/lectures',
+        },
+        {
+            name: 'Quizzes',
+            path: '/quizzes',
+        },
+        {
+            name: 'Constructor',
+            path: '/quizzes/constructor',
+        },
+    ],
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'HIDE': {
+            return { ...state, isShown: false, layoutShown: false };
+        }
+        case 'SHOW': {
+            return { ...state, isShown: true, layoutShown: true };
+        }
+        default:
+            return state;
+    }
+};
+
+export const LayoutContext = createContext();
+
+export const LayoutProvider = ({ children }) => {
+    const value = useReducer(reducer, initialState);
+
+    return (
+        <LayoutContext.Provider value={value}>
+            {children}
+        </LayoutContext.Provider>
+    );
+};
+
+LayoutProvider.propTypes = {
+    children: PropTypes.node,
+};
