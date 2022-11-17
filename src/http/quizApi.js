189bd099b6db7538daf2a7ed530b7cb52df68c0e
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:54:44 +0100

    refactor: names, tabs and quotes

--- src/http/quizApi.js
+++ src/http/quizApi.js
@@ -1,31 +1,37 @@
-import { $authHost, $host } from "./index";
+import { $authHost, $host } from './index';
 
-export const create = async (questions, title) => {
-  const { data } = await $host.post('api/quiz/create', { questions, title })
+export const createQuiz = async (data) => {
+    const response = await $host.post('api/quiz/create', data, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+    });
 
-  return data;
-}
+    return response;
+};
 
-export const update = async (id, questions, title) => {
-  const { data } = await $host.post('api/quiz/update/:id', { id, questions, title })
+export const updateQuiz = async (id, questions, title) => {
+    const response = await $host.post('api/quiz/update/:id', {
+        id,
+        questions,
+        title,
+    });
 
-  return data;
-}
+    return response;
+};
 
-export const get = async (id) => {
-  const { data } = await $host.post('api/quiz/:id', { id })
+export const getQuiz = async (id) => {
+    const response = await $host.post('api/quiz/:id', { id });
 
-  return data;
-}
+    return response;
+};
 
-export const getAll = async () => {
-  const { data } = await $host.post('api/quiz/all');
+export const getAllQuizzes = async () => {
+    const response = await $host.post('api/quiz/all');
 
-  return data;
-}
+    return response;
+};
 
-export const remove = async () => {
-  const { data } = await $authHost.get('api/quiz/remove/' )
+export const removeQuiz = async () => {
+    const response = await $authHost.get('api/quiz/remove/');
 
-  return data;
-}
+    return response;
+};
