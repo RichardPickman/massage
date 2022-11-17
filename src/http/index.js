e86b879641f9581411eaae2c8661ea761142b8ea
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:54:19 +0100

    refactor: tabs and quotes

--- src/http/index.js
+++ src/http/index.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
-})
+    baseURL: process.env.REACT_APP_API_URL,
+});
 
 export const $authHost = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
-})
+    baseURL: process.env.REACT_APP_API_URL,
+});
 
-const authInterceptor = config => {
-    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
-    return config
-}
+const authInterceptor = (config) => {
+    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`;
+    return config;
+};
 
-$authHost.interceptors.request.use(authInterceptor)
+$authHost.interceptors.request.use(authInterceptor);
