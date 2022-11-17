88388063d0d0454f7cefeb47dbcbbe3f971665e5
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:53:51 +0100

    refactor: tabs and quotes

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,28 +1,36 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const initialState = {
-  user: {},
-  isAuth: '',
-}
+    user: {},
+    isAuth: '',
+};
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case "REGISTER": {
-      return state;
-    }
-    case "LOGIN": {
-      return state;
-    }
-    
-    default: return state;
-  }
-}
+    switch (action.type) {
+        case 'REGISTER': {
+            return state;
+        }
+        case 'LOGIN': {
+            return state;
+        }
 
+        default:
+            return state;
+    }
+};
 
 export const UserContext = createContext();
 
-export const UserProvider = ({children}) => {
-  const value = useReducer(reducer, initialState);
+export const UserProvider = ({ children }) => {
+    const value = useReducer(reducer, initialState);
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
+    return (
+        <UserContext.Provider value={value}>{children}</UserContext.Provider>
+    );
+};
+
+UserProvider.propTypes = {
+    children: PropTypes.element,
+};
