f59798ae372332e8bdffb3aea4c5a1a3e4ea275e
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:47:53 +0100

    refactor:  change ternary operator and tabs

--- src/components/Answer/index.jsx
+++ src/components/Answer/index.jsx
@@ -1,54 +1,91 @@
+import React from 'react';
 import { Button } from '@mui/material';
-import './style.css';
+import PropTypes from 'prop-types';
 
 const Answer = ({ answerProps }) => {
-  const { 
-    answerText, 
-    index, 
-    onSelectAnswer,
-    currentAnswers,
-    correctAnswers,
-    showAnswers,
-    isFinished,
-  } = answerProps;
+    const {
+        answerText,
+        index,
+        onSelectAnswer,
+        currentAnswers,
+        correctAnswers,
+        showAnswers,
+        isFinished,
+    } = answerProps;
 
-  const answered = currentAnswers.includes(index) ? "contained" : "outlined";
+    const answered = currentAnswers.includes(index) ? 'contained' : 'outlined';
 
-  if (showAnswers) {
-    if (correctAnswers.includes(index)) {
-      return <Button variant="outlined" color="success">{answerText}</Button>
-    }
-    if (!correctAnswers.includes(index)) {
-      return <Button variant="outlined" disabled>{answerText}</Button>
-    }
-  }
-
-  if (isFinished) {
-    if (correctAnswers.includes(index)) {
-      return currentAnswers.includes(index) ?  
-      <Button variant="contained" color="success">{answerText}</Button> :
-      <Button variant="outlined" color="success">{answerText}</Button>
-    }
-    if (!correctAnswers.includes(index) && currentAnswers.includes(index)) {
-      return currentAnswers.includes(index) ?  
-      <Button variant="contained" color="error">{answerText}</Button> :
-      <Button variant="outlined" color="error">{answerText}</Button>
+    if (showAnswers) {
+        if (correctAnswers.includes(index)) {
+            return (
+                <Button fullWidth variant="outlined" color="success">
+                    {answerText}
+                </Button>
+            );
+        }
+        if (!correctAnswers.includes(index)) {
+            return (
+                <Button fullWidth variant="outlined" disabled>
+                    {answerText}
+                </Button>
+            );
+        }
     }
 
-    if (!currentAnswers.includes(index)) {
-      return <Button variant="outlined" disabled>{answerText}</Button>
+    if (isFinished) {
+        if (correctAnswers.includes(index)) {
+            return (
+                <Button
+                    fullWidth
+                    variant={
+                        currentAnswers.includes(index)
+                            ? 'contained'
+                            : 'outlined'
+                    }
+                    color="success"
+                >
+                    {answerText}
+                </Button>
+            );
+        }
+        if (!correctAnswers.includes(index) && currentAnswers.includes(index)) {
+            return (
+                <Button
+                    fullWidth
+                    variant={
+                        currentAnswers.includes(index)
+                            ? 'contained'
+                            : 'outlined'
+                    }
+                    color="error"
+                >
+                    {answerText}
+                </Button>
+            );
+        }
+
+        if (!currentAnswers.includes(index)) {
+            return (
+                <Button fullWidth variant="outlined" disabled>
+                    {answerText}
+                </Button>
+            );
+        }
     }
 
-  }
+    return (
+        <Button
+            fullWidth
+            variant={answered}
+            onClick={() => onSelectAnswer(index)}
+        >
+            {answerText}
+        </Button>
+    );
+};
 
-  return (
-    <Button 
-      variant={answered} 
-      onClick={() => onSelectAnswer(index)}
-    >
-      {answerText}
-    </Button>
-  );  
-}
+export default Answer;
 
-export default  Answer;
+Answer.propTypes = {
+    answerProps: PropTypes.object,
+};
