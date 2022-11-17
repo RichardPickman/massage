272b083ca32a5acb90a9ee4e291c7b347ce70611
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:48:31 +0100

    feat: add new providers

--- src/components/AppContextProvider/index.js
+++ src/components/AppContextProvider/index.js
@@ -1,13 +1,10 @@
+import { ImagesProvider } from '../../context/images';
+import { LayoutProvider } from '../../context/layout';
 import { QuizProvider } from '../../context/quiz';
-import { ConstructorProvider } from '../../context/quizConstructor';
 import { UserProvider } from '../../context/userContext';
 
 import { combineComponents } from '../../utils/combineComponents';
 
-const providers = [
-  UserProvider,
-  ConstructorProvider,
-  QuizProvider,
-]
+const providers = [UserProvider, LayoutProvider, QuizProvider, ImagesProvider];
 
 export const AppContextProvider = combineComponents(...providers);
