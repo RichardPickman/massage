5be1ed4d3b6d9cacb0029a9e5e7cd405a09100e9
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:57:50 +0100

    feat:  add QuizConstructor helpers

--- src/pages/QuizConstructor/helpers.jsx
+++ src/pages/QuizConstructor/helpers.jsx
@@ -1,3 +1,17 @@
-import { QuestionTemplate } from "./template";
+export const questionTemp = {
+    question: '',
+    img: '',
+    answers: [''],
+    correctAnswers: [],
+    isPreview: false,
+};
 
-export const templateWithKey = (amount) => Array.from(Array(amount), (_, i) => <QuestionTemplate key={i} />);
+export const getQuestionFormData = async (data) => {
+    const question = new FormData();
+
+    for (let key of Object.keys(data)) {
+        question.append(key, data[key]);
+    }
+
+    return question;
+};
