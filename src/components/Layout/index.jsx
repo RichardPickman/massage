6d07ad5463e931d4bf37a003b8d5b6c930ad2773
RichardPickman <drezzerock@gmail.com>
2022-11-17 13:50:06 +0100

    feat: add layout component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,20 @@
-import { Box, Container } from '@mui/material';
-import React from 'react';
-
+import { Container } from '@mui/material';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { LayoutContext } from '../../context/layout';
 
 const Layout = ({ children }) => {
-  return (
-    <Container maxWidth="sm">
-      {children}
-    </Container>
-  )
-}
+    const [layoutState, dispatch] = useContext(LayoutContext);
+
+    return layoutState.layoutShown ? (
+        <Container maxWidth="md">{children}</Container>
+    ) : (
+        children
+    );
+};
 
 export default Layout;
+
+Layout.propTypes = {
+    children: PropTypes.element,
+};
