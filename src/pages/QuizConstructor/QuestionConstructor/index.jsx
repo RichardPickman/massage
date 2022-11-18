05da38a62b77e5caa204b0f4b831f380a50465b1
RichardPickman <drezzerock@gmail.com>
2022-11-18 13:19:59 +0100

    feat: re-visited handle state change

--- src/pages/QuizConstructor/QuestionConstructor/index.jsx
+++ src/pages/QuizConstructor/QuestionConstructor/index.jsx
@@ -3,34 +3,37 @@ import AnswerWithCheckbox from '../../../components/AnswerWithCheckbox';
 import React, { memo, useState } from 'react';
 import FileUpload from '../../../components/FileUpload';
 import PropTypes from 'prop-types';
-import {
-    fetchAnswers,
-    fetchCorrectAnswers,
-    fetchImage,
-    fetchQuestion,
-} from './fetch';
 
-const QuestionConstructor = ({ questionId }) => {
+const QuestionConstructor = ({
+    questionId,
+    updateQuestion,
+    removeQuestion,
+}) => {
     const [question, setQuestion] = useState('');
     const [answers, setAnswers] = useState(['']);
     const [image, setImage] = useState(null);
     const [correctAnswers, setCorrectAnswers] = useState([]);
 
-    const addAnswer = async (answer, answerId) => {
-        const fetch = await fetchAnswers(answer, answerId, answers, questionId);
-
-        setAnswers(fetch);
-        addBlank(answerId);
-    };
-
-    const handleCorrectAnswer = async (index) => {
-        const fetch = await fetchCorrectAnswers(
-            correctAnswers,
-            index,
-            questionId
+    const addAnswer = (answer, answerIndex) => {
+        const answersCopy = answers.map((item, index) =>
+            index === answerIndex ? answer : item
         );
 
-        setCorrectAnswers(fetch);
+        setAnswers(answersCopy);
+        addBlank(answerIndex);
+
+        updateQuestion(questionId, { answers: answersCopy });
+    };
+
+    const handleCorrectAnswer = (index) => {
+        const isExist = correctAnswers.includes(index);
+        const correctAnswersCopy = isExist
+            ? correctAnswers.filter((item) => item !== index)
+            : [...correctAnswers, index];
+
+        setCorrectAnswers(correctAnswersCopy);
+
+        updateQuestion(questionId, { correctAnswers: correctAnswersCopy });
     };
 
     const addBlank = (index) =>
@@ -39,17 +42,16 @@ const QuestionConstructor = ({ questionId }) => {
     const removeBlank = () => setAnswers(answers.slice(0, answers.length - 1));
 
     const handleImage = async (event) => {
-        const fetch = await fetchImage(event, questionId);
+        setImage(event.target.files[0]);
 
-        setImage(fetch);
+        updateQuestion(questionId, { img: event.target.files[0] });
     };
 
     const removeImage = () => setImage(null);
 
     const handleQuestion = (event) => {
         setQuestion(event.target.value);
-
-        fetchQuestion(event, questionId);
+        updateQuestion(questionId, { question: question });
     };
 
     return (
@@ -73,7 +75,7 @@ const QuestionConstructor = ({ questionId }) => {
                 onChange={handleQuestion}
             />
             <FileUpload
-                image={image ? image.url : null}
+                image={image ? window.URL.createObjectURL(image) : null}
                 handleImage={handleImage}
                 removeImage={removeImage}
             />
@@ -82,7 +84,7 @@ const QuestionConstructor = ({ questionId }) => {
                     <Grid item key={i} xs={6}>
                         <AnswerWithCheckbox
                             questionId={questionId}
-                            answerId={i}
+                            answerIndex={i}
                             removeBlank={removeBlank}
                             addAnswer={addAnswer}
                             handleCorrectAnswer={handleCorrectAnswer}
@@ -100,4 +102,6 @@ export default memo(QuestionConstructor);
 
 QuestionConstructor.propTypes = {
     questionId: PropTypes.string,
+    updateQuestion: PropTypes.func,
+    removeQuestion: PropTypes.func,
 };
