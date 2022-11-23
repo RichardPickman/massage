1a7a2bc9252ef9ec2b7eef63349505dc2af9e91b
RichardPickman <drezzerock@gmail.com>
2022-11-23 09:30:44 +0100

    refactor: change image handler

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -12,13 +12,25 @@ import ButtonHandler from '../ButtonHandler';
 const Question = ({ currentState, currentQuestion, onSelect }) => {
     return (
         <>
-            {currentQuestion.img && (
+            {typeof currentQuestion.img === 'string' ? (
                 <CardMedia
                     component="img"
                     loading="lazy"
                     height="400px"
                     alt={currentQuestion.question}
                     image={currentQuestion.img}
+                />
+            ) : (
+                <CardMedia
+                    component="img"
+                    loading="lazy"
+                    sx={{
+                        height: '400px',
+                        background: '#000000',
+                        objectFit: 'contain',
+                    }}
+                    alt={currentQuestion.question}
+                    image={window.URL.createObjectURL(currentQuestion.img)}
                 />
             )}
             {currentQuestion.question && (
@@ -30,20 +42,26 @@ const Question = ({ currentState, currentQuestion, onSelect }) => {
             )}
             <CardActions>
                 <Grid container spacing={2}>
-                    {currentQuestion.answers.map((answer) => (
-                        <Grid item key={answer.id} xs={6}>
-                            <ButtonHandler
-                                state={{
-                                    correctAnswers: currentState.correctAnswers,
-                                    currentAnswers: currentState.currentAnswers,
-                                    isFinished: currentState.isFinished,
-                                    showAnswers: currentState.showAnswers,
-                                    onSelect: onSelect,
-                                    answer: answer,
-                                }}
-                            />
-                        </Grid>
-                    ))}
+                    {currentQuestion.answers.map(
+                        (answer) =>
+                            answer.text && (
+                                <Grid item key={answer.id} xs={6}>
+                                    <ButtonHandler
+                                        state={{
+                                            correctAnswers:
+                                                currentState.correctAnswers,
+                                            currentAnswers:
+                                                currentState.currentAnswers,
+                                            isFinished: currentState.isFinished,
+                                            showAnswers:
+                                                currentState.showAnswers,
+                                            onSelect: onSelect,
+                                            answer: answer,
+                                        }}
+                                    />
+                                </Grid>
+                            )
+                    )}
                 </Grid>
             </CardActions>
         </>
