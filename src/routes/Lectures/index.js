9935d1637246d9f4c9105cb5457f2d6d3d8475f6
RichardPickman <drezzerock@gmail.com>
2022-11-24 18:57:16 +0100

    feat: replace fetcher in hook to loader

--- src/routes/Lectures/index.js
+++ src/routes/Lectures/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Lectures from '../../pages/Lectures';
+import Lectures, { loader as lecturesLoader } from '../../pages/Lectures';
 import AddLecture from '../../pages/AddLecture';
 import Lecture, { loader as lectureLoader } from '../../pages/Lecture';
 
@@ -9,6 +9,7 @@ export const lectureRouter = {
         {
             index: true,
             element: <Lectures />,
+            loader: lecturesLoader,
         },
         {
             path: ':id',
