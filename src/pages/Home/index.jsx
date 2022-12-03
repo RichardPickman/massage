f63c68fc8e9ce74a56d23596eef8e98d0ddda38e
RichardPickman <drezzerock@gmail.com>
2022-12-03 18:18:16 +0100

    feat: add header to page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
+import { Typography } from '@mui/material';
 import React from 'react';
+import Header from '../../components/Header';
 
 const Home = () => {
-    return <h1> HOME </h1>;
+    return (
+        <>
+            <Header />
+            <Typography variant="h1"> HOME </Typography>
+        </>
+    );
 };
 
 export default Home;
