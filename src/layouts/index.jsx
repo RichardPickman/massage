cff9535c6d93a756d6e5d1590c82d80aedffb530
RichardPickman <drezzerock@gmail.com>
2023-01-03 15:15:25 +0100

    refactor: add responsiveness

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
 import Wrapper from '../components/Wrapper';
 
 function Layout() {
-    return (
-        <Wrapper>
-            <Box display="flex" flexDirection="column">
-                <Outlet />
-            </Box>
-        </Wrapper>
-    );
+    const { innerWidth } = window;
+
+    if (innerWidth >= 1680) {
+        return (
+            <Wrapper>
+                <Container maxWidth="lg">
+                    <Box>
+                        <Outlet />
+                    </Box>
+                </Container>
+            </Wrapper>
+        );
+    }
+
+    if (innerWidth <= 1680 && innerWidth >= 768) {
+        return (
+            <Wrapper>
+                <Container maxWidth="md">
+                    <Box>
+                        <Outlet />
+                    </Box>
+                </Container>
+            </Wrapper>
+        );
+    }
+
+    if (innerWidth <= 768 && innerWidth >= 360) {
+        return (
+            <Wrapper>
+                <Container maxWidth="lg">
+                    <Box>
+                        <Outlet />
+                    </Box>
+                </Container>
+            </Wrapper>
+        );
+    }
 }
 
 export default Layout;
