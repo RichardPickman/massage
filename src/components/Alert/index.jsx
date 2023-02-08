734b449febe62a13131e8e26c34de0c0ed97a996
RichardPickman <drezzerock@gmail.com>
2023-02-08 01:31:44 +0100

    refactor: change to more readable code

--- src/components/Alert/index.jsx
+++ src/components/Alert/index.jsx
@@ -1,34 +1,21 @@
-import { Button, Link, Alert as AlertStyle, Typography } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import { Alert as StyledAlert } from '@mui/material';
 import React from 'react';
+import { alertStatuses } from '../../utils/consts';
 import PropTypes from 'prop-types';
 
-export default function Alert({ status, onClose, path, text, title }) {
-    return status === 'successful' ? (
-        <AlertStyle
-            severity="success"
-            action={
-                <Button size="small">
-                    <Link underline="none" to={path} component={RouterLink}>
-                        {title}
-                    </Link>
-                </Button>
-            }
-            onClose={onClose}
-        >
-            <Typography variant="body1">{text}</Typography>
-        </AlertStyle>
-    ) : (
-        <AlertStyle severity="error" onClose={onClose}>
-            <Typography variant="body1">{text}</Typography>
-        </AlertStyle>
-    );
+function Alert({ status, text }) {
+    if (status === alertStatuses.SUCCESS) {
+        return <StyledAlert severity="success">{text}</StyledAlert>;
+    }
+
+    if (status === alertStatuses.FAIL) {
+        return <StyledAlert severity="error">{text}</StyledAlert>;
+    }
 }
 
 Alert.propTypes = {
     status: PropTypes.string,
-    onClose: PropTypes.func,
-    path: PropTypes.string,
     text: PropTypes.string,
-    title: PropTypes.string,
 };
+
+export default Alert;
