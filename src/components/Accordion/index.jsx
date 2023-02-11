ae8911e681c4c08f958f6e454a43081628884e50
RichardPickman <drezzerock@gmail.com>
2023-02-11 10:14:49 +0100

    feat: remove logic from accordion

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,88 +1,26 @@
-import React, { useState } from 'react';
-import { ExpandMoreOutlined } from '@mui/icons-material';
-import {
-    StyledAccordion,
-    StyledAccordionDetails,
-    StyledAccordionSummary,
-} from './styles';
-import { IconButton, Typography } from '@mui/material';
+import React, { memo, useState } from 'react';
+import { StyledAccordion } from './styles';
 import PropTypes from 'prop-types';
-import { Box } from '@mui/system';
-import { useAutoAnimate } from '@formkit/auto-animate/react';
-import { Item } from '../../pages/AddMassage/components/helpers';
 
-import ClearIcon from '@mui/icons-material/Clear';
-import SortableTechnicItem from '../../pages/AddMassage/components/SortableTechnics';
-
-function Accordion({ isConstructor, removeGrip, technic, index, moveTechnic }) {
+function Accordion({ children }) {
     const [expanded, setExpanded] = useState(true);
-    const [listRef] = useAutoAnimate();
-    const isGripsNotEmpty = technic.grips.length > 0;
 
-    const handleExpand = (e, isExpanded) =>
-        isGripsNotEmpty && setExpanded(isExpanded);
-
-    const Summary = () => (
-        <StyledAccordionSummary
-            expandIcon={isGripsNotEmpty ? <ExpandMoreOutlined /> : false}
-        >
-            <Typography textTransform={'capitalize'}>
-                {technic.title}
-            </Typography>
-        </StyledAccordionSummary>
-    );
+    const handleExpand = (e, isExpanded) => setExpanded(isExpanded);
 
     return (
         <StyledAccordion expanded={expanded} onChange={handleExpand}>
-            {isConstructor ? (
-                <SortableTechnicItem
-                    technic={technic}
-                    index={index}
-                    accept={technic.acceptsSelf}
-                    moveTechnic={moveTechnic}
-                >
-                    <Summary />
-                </SortableTechnicItem>
-            ) : (
-                <Summary />
-            )}
-            {isGripsNotEmpty && (
-                <StyledAccordionDetails>
-                    <Box
-                        display="flex"
-                        flexDirection="column"
-                        gap={1}
-                        ref={listRef}
-                    >
-                        {technic.grips.map((item, index) =>
-                            isConstructor ? (
-                                <Item key={index} elevation={2}>
-                                    <Typography noWrap>{item.text}</Typography>
-                                    <IconButton
-                                        size="small"
-                                        onClick={() => removeGrip(index)}
-                                    >
-                                        <ClearIcon sx={{ fontSize: 18 }} />
-                                    </IconButton>
-                                </Item>
-                            ) : (
-                                <Typography key={index}>{item.text}</Typography>
-                            )
-                        )}
-                    </Box>
-                </StyledAccordionDetails>
-            )}
+            {children}
         </StyledAccordion>
     );
 }
 
-export default Accordion;
+export default memo(Accordion);
 
 Accordion.propTypes = {
     technic: PropTypes.object,
-    moveTechnic: PropTypes.func,
-    index: PropTypes.number,
+    technicIndex: PropTypes.number,
     removeItem: PropTypes.func,
     isConstructor: PropTypes.bool,
     removeGrip: PropTypes.func,
+    children: PropTypes.node,
 };
